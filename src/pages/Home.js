b469b2f4457c96661940491b712e07c598f8a35e
ArberSh <arbershaska2@gmail.com>
2024-06-08 17:46:22 +0200

    Fixed Image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import AppsIcon from "@mui/icons-material/Apps";
 import Avatar from "@mui/material/Avatar";
 import { deepOrange, deepPurple } from "@mui/material/colors";
 import Search from "./Search";
+import GoogleImg from "../assets/Google.png"
 
 export default function Home() {
   return (
@@ -23,7 +24,7 @@ export default function Home() {
       </div>
 
       <div className="home__body">
-        <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png"></img>
+        <img src={GoogleImg}></img>
       </div>
       <div className="home__inputContainer">
         <Search />
